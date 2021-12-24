955aec10076ef07cd784f911c72f86d7d73b0bbc
Jenna <jenna48071@gmail.com>
2021-12-24 15:11:42 -0600

    gray out background when form opens

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+const pageContainer = document.getElementById('container');
 const addBookBtn = document.getElementById('addBookBtn');
 const formContainer = document.getElementById('form-container');
 const closeBtn = document.getElementById('closeBtn');
 
 addBookBtn.addEventListener('click', function(){
     formContainer.classList.add('active');
+    pageContainer.classList.add('inactive');
 });
 
 closeBtn.addEventListener('click', function(){
     formContainer.classList.remove('active');
+    pageContainer.classList.remove('inactive');
 });
