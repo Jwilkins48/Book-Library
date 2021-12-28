d7635b34292247bf280f3056222007170498bec0
Jenna <jenna48071@gmail.com>
2021-12-28 16:37:03 -0600

    added functioning for add button

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Variables
 const pageContainer = document.getElementById('container');
 const addBookBtn = document.getElementById('addBookBtn');
 const formContainer = document.getElementById('form-container');
@@ -5,59 +6,80 @@ const closeBtn = document.getElementById('closeBtn');
 const addBtn = document.querySelector('#submitBtn');
 const bookContainer = document.getElementById('book-container');
 
-let checkbox = document.getElementById('finishedCheck');
+let readBook = document.querySelector('.readBook');
+let finishedDisplay = document.getElementById('finishedDisplay');
 let bookNameInput = document.querySelector('#bookName');
 let authorNameInput = document.getElementById('authorName');
 let pageAmountInput = document.getElementById('pageAmount');
-let haveReadInput = document.getElementById('haveRead');
+let checkbox = document.getElementById('finishedCheck');
 let inputs = document.querySelectorAll('input');
 let myLibrary = [];
 
-function Book(bookName, authorName, pageAmount, haveRead) {
-    this.bookName = bookName;
-    this.authorName = authorName;
-    this.pageAmount = pageAmount;
-    this.haveRead = haveRead;
+
+
+// Take input and place in object
+let addToLibrary = () => {
+    const title = bookNameInput.value;
+    const author = authorNameInput.value;
+    const pages = pageAmountInput.value;
+    let read = readBook.innerHTML;
+    
+
+    if (title && author && pages) {
+        const newBook = {
+            title,
+            author,
+            pages,
+            read
+        }
+        // if (checkbox.checked === true){
+        //     read.innerHTML = 'read'
+        // }else {
+        //     read.innerHTML = 'Not read'
+        // };
+        createCard(newBook);
+
+        title.value = '';
+        author.value = '';
+        pages.value = '';
+
+        close();
+    } else {
+        alert('Please fill out all fields');
+    };
 };
 
-let addToLibrary = () => {
-    const newBook = new Book(bookNameInput.value, authorNameInput.value, pageAmountInput.value, haveReadInput.value);
-    myLibrary.push(newBook);
-    createCard(newBook);
-}
-
+// Create new book div
 let createCard = (book) => {
     let bookDiv = document.createElement('div');
-    let titleDiv = document.createElement('h3');
-    let authorDiv = document.createElement('h2');
-    let pagesDiv = document.createElement('h2');
-    let readDiv = document.createElement('h2');
-    let deleteBtn = document.createElement('button');
-
-    titleDiv.textContent = bookNameInput.value;
-    authorDiv.textContent = authorNameInput.value;
-    pagesDiv.textContent = pageAmountInput.value;
-
     bookDiv.classList.add('defaultBook');
+
     bookContainer.appendChild(bookDiv);
 
-    bookDiv.innerHTML = `<h1 class="bookTitle">${Book.bookName}</h1>
-    <h2 class="bookAuthor">${Book.authorName}</h2>
-    <h2 class="bookPages">${Book.pageAmount}</h2>
-    <h2 class="readBook">${Book.haveRead}</h2>`
+    bookDiv.innerHTML = `<h1 class="bookTitle">${book.title}</h1>
+    <h2 class="bookAuthor">${book.author}</h2>
+    <h2 class="bookPages">${book.pages}</h2>
+    <h2 class="readBook">${book.read}</h2>`
+
+    myLibrary.push(bookDiv);
+    console.log(myLibrary);
 }
 
-addBtn.addEventListener('click', addToLibrary());
 
+// Close form
+function close() {
+    formContainer.classList.remove('active');
+    pageContainer.classList.remove('inactive');
 
-
+    bookNameInput.value = '';
+    authorNameInput.value = '';
+    pageAmountInput.value = '';
+}
 
 addBookBtn.addEventListener('click', function () {
     formContainer.classList.add('active');
     pageContainer.classList.add('inactive');
 });
 
-closeBtn.addEventListener('click', function () {
-    formContainer.classList.remove('active');
-    pageContainer.classList.remove('inactive');
-});
+closeBtn.addEventListener('click', close);
+addBtn.addEventListener('click', addToLibrary);
